6a8c9daa86a941dedb7587a0f308e3cf7540dde8
homahi <homahi0128@gmail.com>
2018-01-18 21:38:46 +0900

    No255 構造体自身のプロパティを変更する

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -10,26 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    struct Goods {
-        let tanka: Int
-        let kosu: Int
+    struct Line {
+        var p1: Point
+        var p2: Point
         
-        var price:Int{
-            return tanka * kosu
-        }
-        
-        func sellprice(nebiki: Int = 0) -> Int{
-            return price - nebiki * kosu
+        mutating func move(h:Double, v:Double){
+            p1.x += h
+            p1.y += v
+            p2.x += h
+            p2.y += v
         }
     }
+    
+    struct Point {
+        var x: Double
+        var y: Double
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myGoods = Goods(tanka:700, kosu: 4)
-        let selling_price = myGoods.sellprice(nebiki: 10)
+        let point1 = Point(x: 100, y: 100)
+        let point2 = Point(x: 300, y: 200)
         
-        print("一個\(myGoods.tanka)円、\(myGoods.kosu)個入り、定価\(myGoods.price)円")
-        print("販売価格\(selling_price)円")
+        var theLine = Line(p1: point1, p2: point2)
+        
+        print("移動前のp1\(theLine.p1)")
+        print("移動前のp2\(theLine.p2)")
+        print("--------")
+        
+        theLine.move(h:50, v: 60)
+        print("移動前のp1\(theLine.p1)")
+        print("移動前のp2\(theLine.p2)")
     }
     
     override func didReceiveMemoryWarning() {
