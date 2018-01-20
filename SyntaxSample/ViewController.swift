aef89c66c70e045e3637c8a4afe3994090fc1b9b
homahi <homahi0128@gmail.com>
2018-01-20 17:52:36 +0900

    No329 選択肢ボタンを作る

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    @IBOutlet weak var msgLabel: UILabel!
-    @IBAction func showHide(_ sender: UISwitch) {
-        msgLabel.isHidden = !sender.isOn
+    @IBOutlet weak var colorChip: UIView!
+    @IBAction func changedColor(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex{
+        case 0:
+            colorChip.backgroundColor = UIColor.blue
+        case 1:
+            colorChip.backgroundColor = UIColor.yellow
+        case 2:
+            colorChip.backgroundColor = UIColor.green
+        default:
+            colorChip.backgroundColor = UIColor.blue
+            
+        }
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
