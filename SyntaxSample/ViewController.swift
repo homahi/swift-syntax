f45a04c18e4b9897144a0b3703f9d68f4e6445ca
homahi <homahi0128@gmail.com>
2018-01-20 16:46:49 +0900

    No307 画像付きボタン

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -9,22 +9,11 @@
 import UIKit
 class ViewController: UIViewController {
 
-    @IBOutlet weak var numLabel: UILabel!
-    @IBOutlet weak var okButton: UIButton!
-    @IBAction func random(_ sender: Any) {
-        let num = arc4random_uniform(100)
-        numLabel.text = String(num)
-        
-        okButton.isEnabled = (num>=50)
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    @IBAction func tapOK(_ sender: Any) {
-        print("OK")
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
