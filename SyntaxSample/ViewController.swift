5dffe33d8aa88b27ac511f9f065cafa774d58ef5
homahi <homahi0128@gmail.com>
2018-01-21 15:15:01 +0900

    No363 Viewの下にラベルを貼り、親子関係が存在することを確認
    Viewには親子関係が存在する

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -7,16 +7,13 @@
 //
 
 import UIKit
-class ViewController: UIViewController , UIPickerViewDelegate, UIPickerViewDataSource{
-    
-    let compos = [["月","火", "水", "木", "金"],["早朝","午前中", "昼間","夜間"]]
 
-    @IBOutlet weak var myPickerView: UIPickerView!
+class ViewController: UIViewController {
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        myPickerView.delegate = self
-        myPickerView.dataSource = self
 
     }
     
@@ -25,43 +22,5 @@ class ViewController: UIViewController , UIPickerViewDelegate, UIPickerViewDataS
         // Dispose of any resources that can be recreated.
     }
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return compos.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        let compo = compos[component]
-        return compo.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
-        if component == 0 {
-            return 50
-        } else {
-            return 100
-        }
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        let item = compos[component][row]
-        return item
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        // 選ばれた項目
-        let item = compos[component][row]
-        print("\(item)が選ばれた")
-        
-        // 現在選択されている行番号
-        let row1 = pickerView.selectedRow(inComponent: 0)
-        let row2 = pickerView.selectedRow(inComponent: 1)
-        print("現在選択されている行番号")
-        
-        // 現在選択されている項目名
-        let item1 = self.pickerView(pickerView, titleForRow: row1, forComponent: 0)
-        let item2 = self.pickerView(pickerView, titleForRow: row2, forComponent: 1)
-        print("現在選択されている項目名 \(item1!, item2!)")
-        print("---------------------")
-    }
 }
 
