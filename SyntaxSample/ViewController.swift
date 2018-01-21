e561eed1eb428bff5b4dddefe1f3d2db6ececc1b
homahi <homahi0128@gmail.com>
2018-01-21 16:03:09 +0900

    No385 スタックビューを使ってみる

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -10,24 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var arrow: UIImageView!
-    @IBOutlet weak var tombo: UIImageView!
-    @IBOutlet weak var sky: UIView!
-    @IBAction func catchTombo(_ sender: Any) {
-        // skyビューの座標tombo.centerをviewビューの座標系に変換する
-        var point = view.convert(tombo.center, from: sky)
-        // 矢印の先端がpointを指し示すように調整する
-        point.x += arrow.bounds.width/2
-        point.y -= arrow.bounds.height/2
-        // 矢印を移動させる
-        arrow.center = point
-        
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
-
     }
     
     override func didReceiveMemoryWarning() {
