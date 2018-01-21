953e21c60ec9dc3c402d0060844a65c9fd12bdbf
homahi <homahi0128@gmail.com>
2018-01-21 15:23:45 +0900

    No369 画像を表示するイメージViewを作る

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -14,31 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let labelA = UILabel()
-        labelA.text = "ラベルA"
-        labelA.frame = CGRect(x:80, y:150, width:110, height:21)
-        labelA.backgroundColor = UIColor.orange
-        labelA.textColor = UIColor.white
-        
-        self.view.addSubview(labelA)
-        
-        // ビューを作る
-        let theView = UIView()
-        // ビューの領域と背景色
-        theView.frame = CGRect(x:60, y:100, width: 210, height:200)
-        theView.backgroundColor = UIColor(red:0.9, green:0.9, blue:0.6, alpha:1.0)
-        
-        // ラベルBを作る
-        let labelB = UILabel()
-        labelB.text = "ラベルB"
-        // ラベルの領域
-        labelB.frame = CGRect(x:50, y:60, width: 100, height:21)
-        labelB.backgroundColor = UIColor.orange
-        labelB.textColor = UIColor.white
-        
-        // ラベルBをtheViewに追加する
-        theView.addSubview(labelB)
-        self.view.addSubview(theView)
+        let rect = CGRect(x: 0, y: 0, width: 300, height: 200)
+        let imageView = UIImageView(frame:rect)
 
     }
     
