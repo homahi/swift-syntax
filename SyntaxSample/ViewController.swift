6857f9082bdd27388ff23fbe0114351258bc3f65
homahi <homahi0128@gmail.com>
2018-01-21 13:29:43 +0900

    No333 Sliderで画面の透過率を制御する

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -9,19 +9,13 @@
 import UIKit
 class ViewController: UIViewController {
     
-    func RGBA(red:CGFloat, green:CGFloat, blue: CGFloat, alpha:CGFloat) -> UIColor{
-        let r = red/255.0
-        let g = green/255.0
-        let b = blue/255.0
-        let rgba = UIColor(red:r, green:g, blue:b, alpha: alpha)
-        return rgba
+    @IBOutlet weak var homahi: UIImageView!
+    @IBAction func changeSlider(_ sender: UISlider) {
+        homahi.alpha = CGFloat(sender.value)
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let color = RGBA(red:100, green:255, blue: 200, alpha:1)
-        self.view.backgroundColor = color
     }
     
     override func didReceiveMemoryWarning() {
