d90faa8f5cb59f5dc121255e29a3afa2a71af515
homahi <homahi0128@gmail.com>
2018-01-22 19:53:47 +0900

    No393 テーブルにデータを追加する

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -8,15 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let sectionTitle = ["チョウ目", "バッタ目", "コウチュウ目"]
+let section0 = [("キタテハ", "タテハチョウ科"), ("クロアゲハ", "アゲハチョウ科")]
+let section1 = [("キリギリス", "キリギリス科"),("ヒナバッタ", "バッタ科"),("マツムシ","マツムシ科")]
+let section2 = [("ハンミョウ","ハンミョウ科"), ("アオオサムシ","オサムシ科"),("チビクワガタ","クワガタムシ科")]
+let tableData = [section0, section1, section2]
+
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let sectionData = tableData[section]
+        return sectionData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let sectionData = tableData[(indexPath as NSIndexPath).section]
+        let cellData = sectionData[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = cellData.0
+        cell.detailTextLabel?.text = cellData.1
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitle[section]
     }
     
 }
