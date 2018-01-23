f558b3629069fbedf664bc0851f0fb574c349ae3
homahi <homahi0128@gmail.com>
2018-01-23 20:36:38 +0900

    No421 入力がキーボードに被った際に画面をスクロールさせる
    イベントを厳密にキャッチしていかないといけないのでかなり複雑。
    これ実装できるのかな・・・便利なライブラリとかはありそう。

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -8,15 +8,16 @@
 
 import UIKit
 
-
-
 class ViewController: UIViewController , UITextFieldDelegate{
     
     var editingField:UITextField?
+    var lastOffsetY :CGFloat = 0.0
+    var overlap: CGFloat = 0.0
 
     @IBOutlet weak var myScrollView: UIScrollView!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet var myTextFields: [UITextField]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,7 +29,7 @@ class ViewController: UIViewController , UITextFieldDelegate{
         let notification = NotificationCenter.default
         
         // キーボードのframeが変化した
-        notification.addObserver(self,  selector: #selector(ViewController.keybordChangeFrame(_:)), name: NSNotification.Name.UIKeyboardDidChangeFrame, object: nil)
+        notification.addObserver(self,  selector: #selector(ViewController.keyboardChangeFrame(_:)), name: NSNotification.Name.UIKeyboardDidChangeFrame, object: nil)
         
         // キーボードが登場した
         notification.addObserver(self,  selector: #selector(ViewController.keyboardWillShow(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
@@ -52,6 +53,7 @@ class ViewController: UIViewController , UITextFieldDelegate{
     @IBAction func tapView(_ sender: Any) {
         view.endEditing(true)
     }
+    // 編集開始のデリゲートメソッド
     func textFieldDidBeginEditing(_ textField: UITextField) {
         // 編集中のテキストフィールド
         editingField = textField
@@ -64,11 +66,11 @@ class ViewController: UIViewController , UITextFieldDelegate{
         }
         // キーボードのframeを調べる
         let userInfo = (notification as NSNotification).userInfo!
-        let keybordFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
         // テキストフィールドのframeをキーボードと同じ座標系にする
         let fldFrame = view.convert(fld.frame, from:contentView)
         // 編集中のテキストフィールドがキーボードと重なっていないか調べる
-        var overlap = fldFrame.maxY - keybordFrame.minY + 5
+        overlap = fldFrame.maxY - keyboardFrame.minY + 5
         if overlap > 0{
             overlap += myScrollView.contentOffset.y
             myScrollView.setContentOffset(CGPoint(x: 0, y:overlap), animated: true)
@@ -77,13 +79,28 @@ class ViewController: UIViewController , UITextFieldDelegate{
         
     }
     
+    // 改行入力のデリゲートメソッド
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         view.endEditing(true)
         return false
     }
     
+    // 編集終了のデリゲートメソッド
     func textFieldDidEndEditing(_ textField: UITextField) {
         editingField = nil
+    }
+    
+    // キーボードが登場する通知を受けた
+    @objc func keyboardWillShow(_ notification: Notification){
+        // 現在のスクロール量を保存しておく
+        lastOffsetY = myScrollView.contentOffset.y
+    }
+    
+    // キーボードが退場した通知を受けた
+    @objc func keyboardDidHide(_ notification: Notification){
+        // スクロールを元に戻す
+        myScrollView.setContentOffset(CGPoint(x:0, y: lastOffsetY), animated: true)
     }
 }
 
