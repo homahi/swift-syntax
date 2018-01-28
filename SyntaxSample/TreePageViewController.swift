2beae294ad87af33c240afa573cb5df5e09f5f0c
homahi <homahi0128@gmail.com>
2018-01-28 13:43:11 +0900

    No439 今のページを閉じて次のシーンに戻る

---
+++ SyntaxSample/TreePageViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TreePageViewController.swift
+//  SyntaxSample
+//
+//  Created by 原野誉大 on 2018/01/27.
+//  Copyright © 2018年 原野誉大. All rights reserved.
+//
+
+import UIKit
+
+class TreePageViewController: UIViewController {
+
+    @IBAction func backToHome(_ sender: Any) {
+        // 現在のシーンを閉じて元のシーンに戻る
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
