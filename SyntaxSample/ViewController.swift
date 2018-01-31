8882a9488673935ebe046f49849a681795d14270
homahi <homahi0128@gmail.com>
2018-01-31 13:25:58 +0900

    No457 Webページを表示する

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -9,11 +9,26 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController , UITextFieldDelegate{
+class ViewController: UIViewController  {
     
-
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    var data:(name:String, url:String)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // dataが設定されていればwebDataに代入する
+        guard let webData = data else {
+            return
+        }
+        
+        // 表示するWebページのURLRequestを作る
+        let myURL = URL(string: webData.url)
+        let myRequest = URLRequest(url: myURL!)
+        // Webを読み込む
+        webView.load(myRequest)
         
     }
     
