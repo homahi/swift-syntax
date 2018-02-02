59b8d470700678297e25656fe7fc07800edc43bf
homahi <homahi0128@gmail.com>
2018-02-02 16:03:23 +0900

    No543 折れ線グラフを書く

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -67,38 +67,53 @@ class ViewController: UIViewController {
     }
     
     func drawLine() -> UIImage{
+        // 折れ線にする点の配列
+        var pointList = Array<CGPoint>()
+        let pointCount = 20 // 点の個数
+        // xの間隔
+        let dx = Int(view.frame.width)/pointCount
+        // yの振幅
+        let height = UInt32(view.frame.height)/2
+        // 点の配列を作る
+        for n in 1...pointCount {
+            let px = CGFloat(dx * n)
+            let py = CGFloat(arc4random_uniform(height) + 50)
+            let point = CGPoint(x: px, y: py)
+            pointList.append(point)
+        }
+        
         // イメージ処理の開始
         let size = view.bounds.size
         UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
         
-        let percent = 58.2
+        // パスの初期化
+        let drawPath = UIBezierPath()
+        // 始点に移動する
+        drawPath.move(to: pointList[0])
+        // 配列から始点の値を取り除く
+        pointList.removeFirst()
+        // 配列から点を取り出して連結していく
+        for pt in pointList{
+            drawPath.addLine(to: pt)
+        }
         
-        // 円弧のパスを作る
-        UIColor.red.setStroke()
-        let arcPath = arcPercent(80, percent)
-        arcPath.lineWidth = 60
-        arcPath.lineCapStyle = .butt
+        // 線の色
+        UIColor.blue.setStroke()
+        // 線幅
+        drawPath.lineWidth = 2.0
+        // 線のつなぎ目の形状
+        drawPath.lineJoinStyle = .round
+        // 点線にする
+        drawPath.setLineDash([4.0,2.0], count: 2, phase: 0.0)
+        // 線を描く
+        drawPath.stroke()
         
-        // パスを平行移動する
-        let tf = CGAffineTransform(translationX: view.center.x, y: view.center.y)
-        arcPath.apply(tf)
-        arcPath.stroke()
-        
-        // 何％の数字を描く
-        let font = UIFont.boldSystemFont(ofSize: 28)
-        let textFontAttributes = [NSAttributedStringKey.font: font, NSAttributedStringKey.foregroundColor: UIColor.gray]
-        let drawString = String(percent) + "%"
-        let posX = view.center.x - 45
-        let posY = view.center.y - 15
-        
-        let rect  = CGRect(x: posX, y:posY, width: 90, height:30)
-        drawString.draw(in: rect, withAttributes: textFontAttributes)
-        // イメージコンテキストからUIImageを作る
+        // イメージコンテキストからUIImage
         let image = UIGraphicsGetImageFromCurrentImageContext()
         // イメージ処理の終了
         UIGraphicsEndImageContext()
         return image!
-        
+
     }
     
     func arcPercent(_ radius: CGFloat, _ percent:Double) -> UIBezierPath{
@@ -116,20 +131,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 四角形のイメージを作る
-//        let boxImage = makeBoxImage(width: 240, height: 180)
-        let boxImage = makeRoundRectImage(width: 240, height: 180, corner: 30)
-
-        // イメージビューに設定する
-        let boxView = UIImageView(image: boxImage)
-        // 画面に表示する
-        boxView.center = view.center
-        view.addSubview(boxView)
-        
         let drawImage = drawLine()
         let drawView = UIImageView(image: drawImage)
-        // 画面に表示する
+        
         view.addSubview(drawView)
+        
     }
     
     override func didReceiveMemoryWarning() {
