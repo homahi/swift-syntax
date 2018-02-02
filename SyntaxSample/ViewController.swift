8addbfa0dd2b2815ad112fb7ac2e81492879bd01
homahi <homahi0128@gmail.com>
2018-02-02 11:50:40 +0900

    No515 画像をドラッグしているかのようにする
    ビューをドラッグするたびにイベントが発生するので、その値をビューにどんどん加えていけば動いているように見えるらしい。

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -11,26 +11,15 @@ import WebKit
 
 class ViewController: UIViewController {
     
+    @IBAction func dragging(_ sender: UIPanGestureRecognizer) {
+        // ドラッグしているビュー
+        let tombo = sender.view!
+        tombo.center = sender.location(in: self.view)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tapGesture = UITapGestureRecognizer(target: self, action : #selector(self.hello(_:)))
-        // ビューを作る
-        let myView = UIView(frame: CGRect(x: 100, y: 100, width: 80, height:80))
-        myView.backgroundColor = UIColor.green
-        // tagに番号をつける
-        myView.tag = 1
-        // ビューにタップジェスチャーレコグナイザを追加する
-        myView.addGestureRecognizer(tapGesture)
-        view.addSubview(myView)
     }
-    
-    @objc func hello(_ sender:UITapGestureRecognizer){
-        // tagの番号を調べる
-        let tagNo = sender.view?.tag
-        print("ハロー", tagNo!)
-    }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
