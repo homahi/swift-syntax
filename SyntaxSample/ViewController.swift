2e07b399357f56b81248f6407e56774f76453814
homahi <homahi0128@gmail.com>
2018-02-02 11:36:52 +0900

    No507 ビューをタップして画像をランダムに移動させる
    User Interaction Enabledをチェックしていないと動作させることはできない

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -9,54 +9,15 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, UITextFieldDelegate  {
+class ViewController: UIViewController {
     
-    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
-        print("test")
-        // flowerを作る
-        let flower = UIImageView(image: UIImage(named: "homahi"))
-        // 透明度を0にする
-        flower.alpha = 0
-        
-        // 0.2倍サイズにする
-        let scaleTransform = CGAffineTransform(scaleX: 0.2, y:0.2)
-        // -π/2 回転する
-        let rotationTransform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi/2))
-        // トランスフォームを足し合わせる
-        let transform = scaleTransform.concatenating(rotationTransform)
-        // flowerを変形させる
-        flower.transform = transform
-        
-        // タップされた画像にflowerを追加する
-        flower.center = sender.location(in: self.view)
-        view.addSubview(flower)
-        
-        // アニメーション
-        UIView.animate(withDuration: 2.0,
-                       delay: 0,
-                       options: [.curveEaseInOut],
-                       animations:{
-                        flower.alpha = 1.0
-        },
-                       completion: {(finished:Bool) in
-                        self.fadeoutAndRemove(flower)
-        })
-    }
-    
-    func fadeoutAndRemove(_ view:UIView){
-        // フェードアウトする
-        UIView.animate(
-            withDuration: 2.0,
-            delay: 3.0,
-            options: UIViewAnimationOptions(),
-            animations: {
-                view.alpha = 0.0
-        },
-            completion:{(finished:Bool) in
-                // ビューから取り除く
-                view.removeFromSuperview()
-        })
-        
+    @IBAction func tapHomahi(_ sender: UITapGestureRecognizer) {
+        // タップされたとんぼ
+        let tombo = sender.view!
+        // 画面のランダムな座標に移動する
+        let newX = arc4random_uniform(UInt32(view.frame.width))
+        let newY = arc4random_uniform(UInt32(view.frame.height))
+        tombo.center = CGPoint(x: Double(newX), y: Double(newY))
     }
     override func viewDidLoad() {
         super.viewDidLoad()
