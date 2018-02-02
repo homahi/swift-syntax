16d6ee49402b0031cfab8c5c1c54fbbefe911ac4
homahi <homahi0128@gmail.com>
2018-02-02 11:23:00 +0900

    No497 トランスフォームを使った変形

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -11,27 +11,42 @@ import WebKit
 
 class ViewController: UIViewController, UITextFieldDelegate  {
     
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+        print("test")
+        // flowerを作る
+        let flower = UIImageView(image: UIImage(named: "homahi"))
+        // 透明度を0にする
+        flower.alpha = 0
+        
+        // 0.2倍サイズにする
+        let scaleTransform = CGAffineTransform(scaleX: 0.2, y:0.2)
+        // -π/2 回転する
+        let rotationTransform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi/2))
+        // トランスフォームを足し合わせる
+        let transform = scaleTransform.concatenating(rotationTransform)
+        // flowerを変形させる
+        flower.transform = transform
+        
+        // タップされた画像にflowerを追加する
+        flower.center = sender.location(in: self.view)
+        view.addSubview(flower)
+        
+        // アニメーション
+        UIView.animate(withDuration: 1.0,
+                       delay: 0,
+                       options: [.curveEaseInOut],
+                       animations:{
+                        flower.alpha = 1.0
+                        flower.transform = .identity
+        },
+                       completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // アザラシをビューに追加する
-        let homahi = UIImageView(image: UIImage(named:"homahi"))
-        homahi.center = CGPoint(x:50, y:150)
-        view.addSubview(homahi)
-        
-        // 画面を横に行ったり来たりする
-        UIView.animate(withDuration: 1.0,
-                       delay:0,
-                       options:[.curveEaseInOut,
-                                .autoreverse,
-                                .repeat],
-                       animations:{
-                        homahi.center.x = self.view.frame.width - 50
-        },
-                       completion:nil)
     }
     
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
