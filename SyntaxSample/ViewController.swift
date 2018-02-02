34306b0640c743a5326e97c6879fabd0e8280123
homahi <homahi0128@gmail.com>
2018-02-02 12:10:05 +0900

    No521 画像を回転させる
    画像は前回の回転量を保存しておかないと、回転を始めたときに0から計算を始めてしまって見づらくなる。

--- SyntaxSample/ViewController.swift
+++ SyntaxSample/ViewController.swift
@@ -11,34 +11,26 @@ import WebKit
 
 class ViewController: UIViewController {
     
-    var kakudo: CGFloat = 180.0
+
+    @IBOutlet weak var homahi: UIImageView!
     
-    @IBAction func dragColor(_ sender: UIPanGestureRecognizer) {
-        // ドラッグした量を調べる
-        let translation = sender.translation(in: view)
-        // 水平方向の移動量
-        if translation.x > 0 {
-            // 右方向へドラッグしたら色相環を右回り
-            kakudo -= 1.0
-        } else {
-            kakudo += 1.0
+    @IBAction func rotateTombo(_ sender: UIRotationGestureRecognizer) {
+        switch sender.state{
+        case .began:
+            sender.rotation = lastRotation
+        case .changed:
+            // 回転角度にトンボを合わせる
+            homahi.transform = CGAffineTransform(rotationAngle: sender.rotation)
+        case .ended:
+            lastRotation = sender.rotation
+        default:
+            break
         }
-        
-        // 0~360の間で回す
-        if kakudo < 0 {
-            kakudo += 360
-        } else if kakudo>360{
-            kakudo -= 360
-        }
-        
-        // ビューの背景色を変更する
-        let color = UIColor(hue: kakudo/360, saturation:1.0, brightness:1.0,alpha:1.0)
-        view.backgroundColor = color
     }
+    var lastRotation: CGFloat = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(hue: kakudo/360, saturation: 1.0, brightness:1.0, alpha:1.0)
-        
+
     }
     
     override func didReceiveMemoryWarning() {
